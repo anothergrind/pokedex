35ea0a2deed258e43b7d494dd18c8de59b40b157
anothergrind <ozorjikj@gmail.com>
2025-05-20 00:11:34 -0500

    four randomly generated pokemon + generate button + correct styling

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,14 +1,13 @@
 "use client";
 
+import React, { useState } from "react";
 import CardComponent from "./components/CardComponent";
-import React from "react";
-
-// Material UI Components
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Stack from "@mui/material/Stack";
 import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
 import "./styles/page.css";
 
 // defining the properties of a Pokemon
@@ -24,15 +23,30 @@ type Pokemon = {
   }>;
 };
 
-export default async function Page() {
-  // randomizing the pokemon pulled from the API
-  const randomizer = Math.floor(Math.random() * 1025) + 1;
-  const randomizer2 = Math.floor(Math.random() * 1025) + 1;
+async function fetchRandomPokemon(): Promise<Pokemon> {
+  const randomId = Math.floor(Math.random() * 1025) + 1;
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomId}`);
+  return res.json();
+}
 
-  const first_data = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomizer}`);
-  const second = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomizer2}`);
-  const pokemon: Pokemon = await first_data.json();
-  const pokemon2: Pokemon = await second.json();
+export default function Page() {
+  const [pokemons, setPokemons] = React.useState<Pokemon[]>([]);
+
+  // Fetch 4 random Pokemon
+  const generatePokemons = async () => {
+    const results = await Promise.all([
+      fetchRandomPokemon(),
+      fetchRandomPokemon(),
+      fetchRandomPokemon(),
+      fetchRandomPokemon(),
+    ]);
+    setPokemons(results);
+  };
+
+  // Fetch on mount
+  React.useEffect(() => {
+    generatePokemons();
+  }, []);
 
   return (
     <Box className="page-root">
@@ -41,28 +55,27 @@ export default async function Page() {
           Dashboard
         </Typography>
         <Paper elevation={0} className="page-paper">
-          <Typography variant="h5" fontWeight="medium" gutterBottom>
-            Random Generated Pokemon
-          </Typography>
+          <Stack direction="row" alignItems="center" justifyContent="space-between" mb={2}>
+            <Typography variant="h5" fontWeight="medium" gutterBottom>
+              Random Generated Pokemon
+            </Typography>
+            <Button variant="contained" color="primary" onClick={generatePokemons}>
+              Generate
+            </Button>
+          </Stack>
           <Stack direction="row" spacing={4} className="page-stack">
-            <CardComponent
-              card={{
-                name: pokemon.name,
-                image: pokemon.sprites.front_default,
-                types: pokemon.types.map((t) => t.type.name),
-                onLike: () => {},
-                onDislike: () => {},
-              }}
-            />
-            <CardComponent
-              card={{
-                name: pokemon2.name,
-                image: pokemon2.sprites.front_default,
-                types: pokemon2.types.map((t) => t.type.name),
-                onLike: () => {},
-                onDislike: () => {},
-              }}
-            />
+            {pokemons.map((pokemon, idx) => (
+              <CardComponent
+                key={pokemon.name + idx}
+                card={{
+                  name: pokemon.name,
+                  image: pokemon.sprites.front_default,
+                  types: pokemon.types.map((t) => t.type.name),
+                  onLike: () => {},
+                  onDislike: () => {},
+                }}
+              />
+            ))}
           </Stack>
         </Paper>
       </Container>
